65b7d6c628c36842f4bb55aae022e3a338a30578
arkadiuszNowak89 <arekxd89@gmail.com>
2022-04-08 18:12:34 +0200

    zadania

--- 5-jquery/5-menu-boczne-i-efekt-wyjscia/js/main.js
+++ 5-jquery/5-menu-boczne-i-efekt-wyjscia/js/main.js
@@ -5,7 +5,7 @@ $('.hamburger').on('click', function () {
 $('.main-menu-item a').on('click', function (e) {
   e.preventDefault();
   $('.out-animation-wrapper').fadeOut(
-    1000,
+    2000,
     function () {
       window.location.href = $(this).attr('href');
     }.bind(this)
